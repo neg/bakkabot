95a52c01c417e05e0d93302cde7ee68fd81b1b4a
Richard A (rical) <ric41@yahoo.se>
2013-04-10 19:16:28 +0200

    Add offline test script
    Add a test script that wraps some of the eggdrop procedures.
    This simplifies hacking on the bot without actually running it.
    
    Signed-off-by: Richard A (rical) <ric41@yahoo.se>

---
+++ test.tcl
@@ -0,0 +1,29 @@
+#!/usr/bin/tclsh
+
+# Log wrappers
+set log_wrap {
+    "puthelp"
+    "putkick"
+    "putlog"
+    "putserv"
+}
+foreach name $log_wrap {
+    puts "Registering log procedure \"$name\""
+    proc $name {args} {
+        set str {*}$args
+        puts "[lindex [info level 0] 0]: $str"
+    }
+}
+
+# Bind wrapper
+proc bind {type none str do} {
+    global binds
+    dict set binds $str $do
+    puts "Registering bind \"$str -> $do\""
+}
+
+# Source the bot
+source main.tcl
+
+# Test the chicken
+bind::chicken myuser myhost myhand mychan mytest
