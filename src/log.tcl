3555fac5f28af92c3f2c278e8eb0beacfe1e69be
Richard A (rical) <rical@epoch70.com>
2013-12-29 18:26:44 +0100

    Use putlog instead of puts in log::debug
    Signed-off-by: Richard A (rical) <rical@epoch70.com>

--- src/log.tcl
+++ src/log.tcl
@@ -2,15 +2,16 @@ namespace eval log { namespace export * }
 
 proc log::debug {msg} {
     global PRINT_DEBUG
+
     if {!$PRINT_DEBUG} {
         return
     }
 
     if {[info level] > 1} {
         set caller [lindex [info level [expr [info level] - 1]] 0]
-        puts "debug $caller, $msg"
+        putlog "debug $caller, $msg"
     } else {
-        puts "debug, $msg"
+        putlog "debug, $msg"
     }
 }
 
