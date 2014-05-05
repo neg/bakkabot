e35f7186885445bb9a5471ad142c63b6e461ac3d
Richard A (rical) <rical@epoch70.com>
2014-05-05 10:19:54 +0200

    Add ai blacklisting with voting
    Add the ability to vote for blacklisting of an AI topic.
    
    (This commit also does some sneaky refactoring)
    
    Signed-off-by: Richard A (rical) <rical@highwind.se>

--- main.tcl
+++ main.tcl
@@ -15,13 +15,16 @@ bind pub - "!bakka" chicken::bind
 bind pub - "!streck" streck::add
 bind pub - "!strecklist" streck::list
 bind pub - "!strecktop" streck::top
-bind pub - "!stupid" ai::stupid
+bind pub - "!agree" stage::agree
+bind pub - "!disagree" stage::disagree
+bind pub - "!ai" ai::command
 bind pubm - * catch_all
 
 proc catch_all {nick uhost hand chan text} {
     friday::bind $nick $chan
     streck::checkbad $nick $chan $text
-    ai::bind $nick $chan $text
+    ai::talk $nick $chan $text
+    ai::learn $nick $chan $text
 }
 
 puts "sourced main.tcl (bakka)"
--- src/ai.tcl
+++ src/ai.tcl
@@ -4,6 +4,7 @@ namespace eval ai::int { namespace export * }
 set ai::db_path "$SCRIPT_PATH/db/ai"
 set ai::brainfile "$ai::db_path/brain"
 set ai::stupidfile "$ai::db_path/stupid"
+set ai::blacklistfile "$ai::db_path/blacklist"
 # The Swedish word for "is". TODO: make a list of marks
 set ai::mark "är"
 set ai::last_statement {}
@@ -11,6 +12,7 @@ set ai::last_statement {}
 # Initialize
 misc::mkdir $ai::db_path
 set ai::brain [misc::slurp_file $ai::brainfile]
+set ai::blacklist [misc::slurp_file $ai::blacklistfile]
 
 proc ai::int::remove {topic index} {
     log::debug "removing \"$topic\" ($index) \"[lindex [dict get $ai::brain $topic] $index]\""
@@ -24,26 +26,25 @@ proc ai::int::remove {topic index} {
     misc::dump_data $ai::brain $ai::brainfile
 }
 
-proc ai::learn {match text} {
+proc ai::int::learn {mark topic text} {
 	set breaks [lsearch -all -regexp $text {[\.|!|\\?]}]
     set end [llength $text]
 
-    # Find the first break after the match (if present)
+    # Find the first break after the mark (if present)
     foreach break $breaks {
-        if {$break > $match} {
+        if {$break > $mark} {
             set end $break
         }
     }
 
-    set topic [lindex $text $match-1]
-    set opinion [lrange $text $match $end]
+    set opinion [lrange $text $mark $end]
 
     log::debug "new opinion about \"$topic\" : \"$opinion\""
     dict lappend ai::brain $topic $opinion
     misc::dump_data $ai::brain $ai::brainfile
 }
 
-proc ai::talk {nick chan topic} {
+proc ai::int::talk {nick chan topic} {
     set selected [misc::lrandom_index [dict get $ai::brain $topic]]
     set opinion [lindex [dict get $ai::brain $topic] $selected]
 
@@ -52,9 +53,52 @@ proc ai::talk {nick chan topic} {
     set ai::last_statement [dict create $topic $selected]
 }
 
-proc ai::stupid {nick uhost hand chan text} {
+proc ai::int::blacklist {nick chan word} {
+    dict unset ai::brain $word
+    misc::dump_data $ai::brain $ai::brainfile
+
+    lappend ai::blacklist $word
+    misc::dump_data $ai::blacklist $ai::blacklistfile
+
+    putserv "PRIVMSG $chan :$nick: blacklisted topic \"$word\""
+}
+
+proc ai::command {nick host hand chan text} {
+    set cmd [lindex $text 0]
+    set op [lrange $text 1 end]
+
+    switch $cmd {
+        "blacklist" {
+            ai::blacklist $nick $host $chan [lindex $op 0]
+        }
+        "stupid" {
+            ai::stupid $nick $chan $op
+        }
+        default {
+            putserv "PRIVMSG $chan :$nick: Unknown ai command \"$cmd\""
+        }
+    }
+}
+
+proc ai::blacklist {nick host chan word} {
+
+    if {[lsearch $ai::blacklist $word] >= 0} {
+        putserv "PRIVMSG $chan :$nick: \"$word\" already blacklisted"
+        return
+    }
+    if {[lsearch $ai::mark $word] >= 0} {
+        putserv "PRIVMSG $chan :$nick: \"$word\" naah .."
+        return
+    }
+
+    stage::stage $nick $host $chan "ai blacklist of topic \"$word\"" 1 1 \
+    [list ai::int::blacklist $nick $chan $word]
+
+}
+
+proc ai::stupid {nick chan text} {
     if {[llength $ai::last_statement] != 2} {
-        putserv "PRIVMSG $chan :$nick: I don't recall saying anything"
+        putserv "PRIVMSG $chan :$nick: I don't recall saying anything?"
         return
     }
 
@@ -78,24 +122,36 @@ proc ai::stupid {nick uhost hand chan text} {
 
     ai::int::remove $topic $index
 
-    putserv "PRIVMSG $chan :$nick: alright, reported and forgotten.."
+    putserv "PRIVMSG $chan :$nick: alright, opinion reported and forgotten.."
     set ai::last_statement {}
 }
 
-proc ai::bind {nick chan text} {
+proc ai::learn {nick chan text} {
     set text [string tolower $text]
 
+	# Search for AI mark
 	set mark [lsearch $text $ai::mark]
-    if {$mark > 0} {
-        ai::learn $mark $text
+    set topic [lindex $text $mark-1]
+    if {($mark > 0) && ([lsearch $ai::blacklist $topic] < 0)} {
+        ai::int::learn $mark $topic $text
+    }
+}
+
+proc ai::talk {nick chan text} {
+    set text [string tolower $text]
+
+    # Only talk on friday, TODO: Break this out and align with friday::
+    set day [clock format [clock seconds] -format "%w"]
+    if {$day != 5} {
         return
     }
 
+    # Check if we know any topic in the text (sentence)
     foreach topic [dict keys $ai::brain] {
-        set match [lsearch $text $topic]
-
-        if {$match >= 0} {
-            ai::talk $nick $chan [lindex $text $match]
+        set mark [lsearch $text $topic]
+        if {$mark >= 0} {
+            ai::int::talk $nick $chan [lindex $text $mark]
+            return
         }
     }
 }
